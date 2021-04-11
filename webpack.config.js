47244f148229b635dce93e715b6489d3dc4641cc
GiaDieu <giadieuly@gmail.com>
2021-04-12 00:53:43 +0700

    load images

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,38 @@ module.exports = {
     mode: mode,
     target: target,
     target: 'web',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[hash][ext][query]',
+    },
     module: {
         rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset',
+                generator: {
+                    filename: 'static/[hash][ext][query]',
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 30 * 1024,
+                    },
+                },
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: '' },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
@@ -21,18 +51,12 @@ module.exports = {
                     loader: 'babel-loader',
                 },
             },
-            {
-                test: /\.(s[ac]|c)ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
         ],
     },
     plugins: [new MiniCssExtractPlugin()],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     devtool: 'source-map',
     devServer: {
         contentBase: './dist',
